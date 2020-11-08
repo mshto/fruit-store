ae8efe50f2fd6bb32ac5b91920dcd01df879134c
mshtohryn <m.shtohryn@connectwise.com>
2020-11-08 22:19:47 +0200

    remove files

--- sql/keyspace.sql
+++
@@ -1,1 +0,0 @@
-CREATE DATABASE fruit_store;
--- sql/migrations/1604783298_create_users.up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE users (
-    id uuid DEFAULT uuid_generate_v1(),
-    username VARCHAR(34) NOT NULL UNIQUE,
-    password TEXT NOT NULL,
-    PRIMARY KEY (id)
-);
